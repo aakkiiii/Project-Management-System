a6565114bb3a659a1ed19d20425e0cba901481a9
aakkiiii <arpitm0014@gmail.com>
2026-08-10 21:07:44 +0530

    create , update and delete controllers added.

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -9,13 +9,65 @@ import {
   emailVerificationMailgenContent,
   forgotPasswordMailgenContent,
 } from "../utils/mail.js";
+import mongoose from "mongoose";
+import { UserRolesEnum } from "../utils/constants.js";
 
 const getProject = asyncHandler(async (req, res) => {});
 
-const createProject = asyncHandler(async (req, res) => {});
-const updateProject = asyncHandler(async (req, res) => {});
-const deleteProject = asyncHandler(async (req, res) => {});
-const addMembersToProject = asyncHandler(async (req, res) => {});
+const createProject = asyncHandler(async (req, res) => {
+  const { name, description } = req.body;
+
+  await Project.create({
+    name,
+    description,
+    createdBy: new mongoose.Types.ObjectId(req.user._id),
+  });
+
+  await ProjectMember.create({
+    user: new mongoose.Types.ObjectId(req.user._id),
+    project: new mongoose.Types.ObjectId(project._id),
+    role: UserRolesEnum.ADMIN,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, project, "Project created Successfully."));
+});
+const updateProject = asyncHandler(async (req, res) => {
+  const { name, description } = req.body;
+  const { projectId } = req.params;
+
+  await Project.findByIdAndUpdate(
+    projectId,
+    {
+      name,
+      description,
+    },
+    { new: true },
+  );
+  if (!project) {
+    throw new ApiResponse(404, "Project not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, project, "Project updated successfully"));
+});
+const deleteProject = asyncHandler(async (req, res) => {
+  const { projectId } = req.params;
+
+  const project = await Project.findByIdAndDelete(projectId);
+  if (!project) {
+    throw new ApiResponse(404, "Project not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, project, "Project deleted successfully"));
+});
+const addMembersToProject = asyncHandler(async (req, res) => {
+  
+});
 const getProjectMembers = asyncHandler(async (req, res) => {});
 const updateMemberRole = asyncHandler(async (req, res) => {});
 const deleteMember = asyncHandler(async (req, res) => {});
